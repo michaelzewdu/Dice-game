292861bdd968158979aa87727e49f4e513959b87
Michael Z <51156098+michaelzewdu@users.noreply.github.com>
2022-12-14 12:56:53 +0300

    feat: added player subject

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+alert("Welcome to Dicee game! lets get you started");
+var playerOneName = prompt("Player one enter your name");
+var playerTwoName = prompt("Player two enter your name");
+
 
 var randomNo1 = Math.random() * 6;
 var randomNo2 = Math.random() * 6;
@@ -17,5 +21,22 @@ if (randomNo2 < 1) {
 console.log(randomNo1);
 console.log(randomNo2);
 
-document.getElementsByClassName("img1")[0].attributes.src.value = "images/dice"+randomNo1+".png";
-document.getElementsByClassName("img2")[0].attributes.src.value = "images/dice"+randomNo2+".png";
+if (playerOneName != null) {
+  document.querySelector("p.playerOne").innerHTML = playerOneName.toString();
+}
+if (playerTwoName != null) {
+  document.querySelector("p.playerTwo").innerHTML = playerTwoName.toString();
+}
+
+
+document.getElementsByClassName("img1")[0].attributes.src.value = "images/dice" + randomNo1 + ".png";
+document.getElementsByClassName("img2")[0].attributes.src.value = "images/dice" + randomNo2 + ".png";
+
+if (randomNo1>randomNo2){
+  document.querySelector('h3').innerHTML = playerOneName.toString() + " wins";
+} else if (randomNo2 > randomNo1) {
+  document.querySelector('h3').innerHTML = playerTwoName.toString() + " wins";
+} else {
+  document.querySelector('h3').innerHTML = " That was a tie. Want to go for another round?";
+
+}
